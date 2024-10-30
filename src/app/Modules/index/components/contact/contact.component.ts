2987c1888fa9c97ffd32f4efd0dc4c1fa73e7077
Pablo <pablolopezlujan01@gmail.com>
2024-10-30 16:31:57 +0100

    Links funcionando, bug fixes, página completada (Faltan imágenes y enlaces de proyectos)

--- src/app/Modules/index/components/contact/contact.component.ts
+++ src/app/Modules/index/components/contact/contact.component.ts
@@ -1,10 +1,35 @@
-import { Component } from '@angular/core';
+declare var bootstrap: any;
+
+import { Component, AfterViewInit } from '@angular/core';
 
 @Component({
   selector: 'app-contact',
   templateUrl: './contact.component.html',
-  styleUrl: './contact.component.scss'
+  styleUrls: ['./contact.component.scss']
 })
-export class ContactComponent {
+export class ContactComponent implements AfterViewInit {
 
+  ngAfterViewInit(): void {
+    const copyEmailBtn = document.getElementById('copyEmailBtnContact');
+    if (copyEmailBtn) {
+      const tooltip = new bootstrap.Tooltip(copyEmailBtn, {
+        title: '¡Copiado!',
+        trigger: 'manual',
+        placement: 'right'
+      });
+
+      copyEmailBtn.addEventListener('click', () => {
+        const email = 'pablolopezlujan01@gmail.com';
+        navigator.clipboard.writeText(email).then(() => {
+          // Muestra el tooltip al hacer clic
+          tooltip.show();
+          setTimeout(() => {
+            tooltip.hide(); // Oculta el tooltip después de 2 segundos
+          }, 2000);
+        }).catch(err => {
+          console.error('Error al copiar el correo al portapapeles: ', err);
+        });
+      });
+    }
+  }
 }
