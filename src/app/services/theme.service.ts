87de2f8f3cb4d9f60ae2911547463d5201936d84
Pablo <pablolopezlujan01@gmail.com>
2024-11-07 16:13:13 +0100

    Proyectos añadidos y fix del modo claro y modo oscuro

--- src/app/services/theme.service.ts
+++ src/app/services/theme.service.ts
@@ -5,33 +5,21 @@ import { Injectable } from '@angular/core';
 })
 export class ThemeService {
   private isDarkTheme = false;
-  private lightThemeColor = "#ffffff"; // Color para modo claro
-  private darkThemeColor = "#161616";  // Color para modo oscuro
 
   constructor() {
     // Leer el tema guardado en localStorage
     const savedTheme = localStorage.getItem('dark-theme');
     this.isDarkTheme = savedTheme === 'true';
     document.body.classList.toggle('dark-theme', this.isDarkTheme);
-    this.updateThemeColor(); // Configurar el color inicial de la barra
   }
 
   toggleTheme(): void {
     this.isDarkTheme = !this.isDarkTheme;
-    localStorage.setItem('dark-theme', this.isDarkTheme.toString());
-    document.body.classList.toggle('dark-theme', this.isDarkTheme);
-    this.updateThemeColor(); // Cambiar el color de la barra al cambiar el tema
+    localStorage.setItem('light-theme', this.isDarkTheme.toString());
+    document.body.classList.toggle('light-theme', this.isDarkTheme);
   }
 
   get currentTheme(): boolean {
     return this.isDarkTheme;
-  }
-
-  private updateThemeColor(): void {
-    const metaThemeColor = document.querySelector("meta[name=theme-color]");
-    if (metaThemeColor) {
-      const color = this.isDarkTheme ? this.darkThemeColor : this.lightThemeColor;
-      metaThemeColor.setAttribute("content", color);
-    }
   }
 }
